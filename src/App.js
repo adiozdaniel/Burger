7817def3d66d4a99bea4301163b7028df5331bb5
adiozdaniel <42722945+adiozdaniel@users.noreply.github.com>
2023-01-02 00:36:15 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="bg-indigo-900 text-3xl text-white" >Just beginning</div>
+        )
+    }
+}
+
+export default App; 
