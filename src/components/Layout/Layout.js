03dc92c9bf957d946b32392d01aa603b74d0851f
adiozdaniel <42722945+adiozdaniel@users.noreply.github.com>
2023-01-03 07:52:03 +0300

    clear

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { Component} from "react";
 import Aux from "../../hoc/Auxillary"
 import "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-    <Aux>
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: false,
+    }
+
+    SideDrawerClosedHandler = ()=> {
+        this.setState({showSideDrawer: false});
+    }
+
+    SideDrawerOpenHandler = ()=> {
+        this.setState({showSideDrawer: true});
+    }
+
+    render() {
+        return (
+             <Aux>
         <div>
-            ToolBar, 
-            SideDrawer, 
-            Backdrop
+            <Toolbar drawerToggle={this.SideDrawerOpenHandler} />
+            <SideDrawer 
+                open={this.state.showSideDrawer} 
+                closed={this.SideDrawerClosedHandler}
+            />
         </div>
         <main className='LayoutContent flex flex-col justify-center items-center' >
-            {props.children}
+            {this.props.children}
         </main>
     </Aux>
-    
-);
-
-export default layout;
+        )
+    }
+}
+export default Layout;
